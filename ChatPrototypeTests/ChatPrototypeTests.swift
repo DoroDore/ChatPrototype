ed0d4a7e67f03fc012aa507ef7a5896c52ba48be
DoroDore <tlyz82@student.cis.eduhk>
2025-05-20 08:19:51 +0800

    Initial Commit

---
+++ ChatPrototypeTests/ChatPrototypeTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatPrototypeTests.swift
+//  ChatPrototypeTests
+//
+//  Created by Theodore Zhu on 5/20/25.
+//
+
+import Testing
+
+struct ChatPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
